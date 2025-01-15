6f9e6f0804b993cb3d78f30931c9ca72c563d8c6
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2025-01-15 17:48:44 +0530

    Create Basics.java

---
+++ Arrays/Basics.java
@@ -0,0 +1,9 @@
+public class  Basics{
+    public static void main(String[] args) {
+        int[] arr={1,2};
+        System.out.println(arr);
+
+        // this prints a hashcode memory refernce
+        
+    }
+}
